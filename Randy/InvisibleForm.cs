dfad4b9cfb69a678a9db73bee60d2eeb95aacefa
Kim Goetzke <kbgoetzke@gmail.com>
2024-04-28 09:56:11 +0100

    Fix bug where hot key stops working when MainForm invisible

---
+++ Randy/InvisibleForm.cs
@@ -0,0 +1,114 @@
+﻿using System.Runtime.InteropServices;
+using Microsoft.Extensions.Logging;
+
+namespace Randy;
+
+/**
+ * This form contains the logic for the hotkey. It is permanently invisible.
+ */
+public class InvisibleForm : Form
+{
+    [DllImport("user32.dll")]
+    private static extern IntPtr GetForegroundWindow();
+
+    [DllImport("user32.dll")]
+    private static extern bool GetWindowRect(IntPtr hWnd, out Rect lpRect);
+
+    [DllImport("user32.dll")]
+    private static extern bool ShowWindow(IntPtr hWnd, int nCmdShow);
+
+    [DllImport("user32.dll")]
+    private static extern bool SetWindowPlacement(IntPtr hWnd, ref WindowPlacement lpwndpl);
+
+    [DllImport("user32.dll")]
+    private static extern IntPtr SendMessage(IntPtr hWnd, uint message, IntPtr wParam, IntPtr lParam);
+
+    [DllImport("user32.dll")]
+    private static extern bool RegisterHotKey(IntPtr hWnd, int id, uint fsModifiers, uint vk);
+
+    [DllImport("user32.dll")]
+    private static extern bool UnregisterHotKey(IntPtr hWnd, int id);
+
+    private const int SwMaximize = 3;
+    private const uint WmPaint = 0x000F;
+    private const int SwShowNormal = 1;
+    private readonly ILogger _logger;
+    private readonly MainForm _mainForm;
+    private int _hotkeyId = 1; // ID for the hotkey
+    private readonly uint _wmHotkey = 0x0312; // Windows message for hotkey
+    private uint _modifierKey = 0x0008;
+    private uint _otherKey = (uint)Keys.Oem5; // Backslash
+
+    public InvisibleForm(ILogger logger, MainForm mainForm)
+    {
+        _logger = logger;
+        _mainForm = mainForm;
+        ShowInTaskbar = false;
+        FormBorderStyle = FormBorderStyle.None;
+        WindowState = FormWindowState.Minimized;
+        Opacity = 0;
+    }
+
+    public void RegisterHotKey(int hotKeyId, uint modifierKey, uint otherKey)
+    {
+        _hotkeyId = hotKeyId;
+        _modifierKey = modifierKey;
+        _otherKey = otherKey;
+        _logger.LogInformation("Registering hotkey");
+        RegisterHotKey(Handle, _hotkeyId, _modifierKey, _otherKey);
+    }
+
+    public void UnregisterHotKey(object? sender, FormClosingEventArgs e)
+    {
+        _logger.LogInformation("Unregistering hotkey");
+        UnregisterHotKey(Handle, _hotkeyId);
+    }
+
+    protected override void WndProc(ref Message m)
+    {
+        base.WndProc(ref m);
+
+        if (m.Msg != _wmHotkey || m.WParam.ToInt32() != _hotkeyId)
+        {
+            return;
+        }
+
+        _logger.LogInformation("Hotkey pressed");
+
+        var window = GetForegroundWindow();
+        if (window == _mainForm.Handle)
+        {
+            _logger.LogWarning("Using hotkey on main form is not allowed, ignoring request");
+            return;
+        }
+        
+        ShowWindow(window, SwMaximize); // Maximize the window
+
+        if (!GetWindowRect(window, out var rect))
+        {
+            _logger.LogWarning("Failed to get window rect");
+            return;
+        }
+
+        // Calculate new window size
+        var newX = rect.Left + 30;
+        var newY = rect.Top + 30;
+        var newWidth = rect.Right - rect.Left - 60;
+        var newHeight = rect.Bottom - rect.Top - 60;
+
+        // Set the new window placement
+        var wp = new WindowPlacement
+        {
+            Length = Marshal.SizeOf(typeof(WindowPlacement)),
+            Flags = 0,
+            ShowCmd = SwShowNormal,
+            PtMaxPosition = new Point(0, 0),
+            PtMinPosition = new Point(-1, -1),
+            RcNormalPosition = new Rectangle(newX, newY, newX + newWidth, newY + newHeight)
+        };
+
+        _logger.LogInformation("Updating window placement");
+        SetWindowPlacement(window, ref wp);
+        SendMessage(window, WmPaint, IntPtr.Zero, IntPtr.Zero); // Force a repaint of the window
+    }
+}
--- Randy/MainForm.cs
+++ Randy/MainForm.cs
@@ -6,36 +6,12 @@ namespace Randy;
 
 public sealed partial class MainForm : Form
 {
-    [DllImport("user32.dll")]
-    private static extern bool RegisterHotKey(IntPtr hWnd, int id, uint fsModifiers, uint vk);
-
-    [DllImport("user32.dll")]
-    private static extern bool UnregisterHotKey(IntPtr hWnd, int id);
-
-    [DllImport("user32.dll")]
-    private static extern IntPtr GetForegroundWindow();
-
-    [DllImport("user32.dll")]
-    private static extern bool GetWindowRect(IntPtr hWnd, out Rect lpRect);
-
-    [DllImport("user32.dll")]
-    private static extern bool ShowWindow(IntPtr hWnd, int nCmdShow);
-
-    [DllImport("user32.dll")]
-    private static extern bool SetWindowPlacement(IntPtr hWnd, ref WindowPlacement lpwndpl);
-    
-    [DllImport("user32.dll")]
-    private static extern IntPtr SendMessage(IntPtr hWnd, uint message, IntPtr wParam, IntPtr lParam);
-
     private const int WindowsKeyModifier = 0x0008;
-    private const int WmHotkey = 0x0312; // Windows message for hotkey
     private const int HotkeyId = 1; // ID for the hotkey
-    private const int SwMaximize = 3;
-    private const uint WmPaint = 0x000F;
-    private const int SwShowNormal = 1;
+    private const Keys HotKey = Keys.Oem5; // Backslash
+
     private const FormBorderStyle DefaultFormStyle = FormBorderStyle.FixedDialog;
     private const string IconFile = "../../../assets/randy.ico";
-    // private Rectangle? _previousSize;
     private readonly NotifyIcon _trayIcon;
     private readonly Timer _timer = new();
     private readonly ILogger<MainForm> _logger;
@@ -44,11 +20,12 @@ public sealed partial class MainForm : Form
     {
         InitializeComponent();
         _logger = logger;
+        var invisibleForm = new InvisibleForm(logger, this);
+        invisibleForm.RegisterHotKey(HotkeyId, WindowsKeyModifier, (uint)HotKey);
         
         // Events
-        Load += RegisterHotKey;
-        FormClosing += UnregisterHotKey;
         Resize += MinimiseOnResize;
+        FormClosing += invisibleForm.UnregisterHotKey;
         Minimise(1000);
         
         // Initialize tray icon
@@ -101,44 +78,6 @@ public sealed partial class MainForm : Form
         var centerPoint = new Point(workingArea.Width / 2, workingArea.Height / 2);
         Location = new Point(centerPoint.X - Size.Width / 2, centerPoint.Y - Size.Height / 2);
     }
-
-    protected override void WndProc(ref Message m)
-    {
-        base.WndProc(ref m);
-
-        if (m.Msg != WmHotkey || m.WParam.ToInt32() != HotkeyId)
-            return;
-
-        var hWnd = GetForegroundWindow();
-        ShowWindow(hWnd, SwMaximize); // Maximize the window
-
-        if (!GetWindowRect(hWnd, out var rect))
-        {
-            _logger.LogWarning("Failed to get window rect");
-            return;
-        }
-        
-        // Calculate new window size
-        var newX = rect.Left + 30;
-        var newY = rect.Top + 30;
-        var newWidth = rect.Right - rect.Left - 60;
-        var newHeight = rect.Bottom - rect.Top - 60;
-
-        // Set the new window placement
-        var wp = new WindowPlacement
-        {
-            Length = Marshal.SizeOf(typeof(WindowPlacement)),
-            Flags = 0,
-            ShowCmd = SwShowNormal,
-            PtMaxPosition = new Point(0, 0),
-            PtMinPosition = new Point(-1, -1),
-            RcNormalPosition = new Rectangle(newX, newY, newX + newWidth, newY + newHeight)
-        };
-
-        _logger.LogInformation("Updating window placement");
-        SetWindowPlacement(hWnd, ref wp);
-        SendMessage(hWnd, WmPaint, IntPtr.Zero, IntPtr.Zero); // Force a repaint of the window
-    }
     
     private void MinimiseOnResize(object? sender, EventArgs e)
     {
@@ -154,18 +93,6 @@ public sealed partial class MainForm : Form
         _timer.Tick += Minimise;
         _timer.Start();
         _logger.LogInformation("Starting timer");
-    }
-
-    private void RegisterHotKey(object? sender, EventArgs e)
-    {
-        _logger.LogInformation("Registering hotkey and start timer");
-        RegisterHotKey(Handle, HotkeyId, WindowsKeyModifier, (uint)Keys.Oem5); // Win + Backslash
-    }
-
-    private void UnregisterHotKey(object? sender, FormClosingEventArgs e)
-    {
-        _logger.LogInformation("Unregistering hotkey");
-        UnregisterHotKey(Handle, HotkeyId);
     }
 
     #region Tray icon context menu actions
